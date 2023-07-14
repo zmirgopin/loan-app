546f541f48f0a2145497806d5dee40b7fb10a132
zmirgopin <zahavagopin@gmail.com>
2023-07-14 12:35:11 -0400

    git commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoanCalculatorComponent } from './loan-calculator/loan-calculator.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'loan-calculator', pathMatch: 'full' },
+  { path: 'loan-calculator', component: LoanCalculatorComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'contact-us', component: ContactUsComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BaseLayoutComponent } from './base-layout/base-layout.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BaseLayoutComponent,
+    AboutUsComponent,
+    ContactUsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
