8d2f5489ca9ac7f6dfff7816a2e8f849c19ed222
zmirgopin <zahavagopin@gmail.com>
2023-07-20 13:37:02 -0400

    git commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,21 @@ import { AppComponent } from './app.component';
 import { BaseLayoutComponent } from './base-layout/base-layout.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
+import { FormsModule } from '@angular/forms';
+import { LoanCalculatorComponent } from './loan-calculator/loan-calculator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BaseLayoutComponent,
+    LoanCalculatorComponent,
     AboutUsComponent,
     ContactUsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/loan-calculator/loan-calculator.component.ts
+++ src/app/loan-calculator/loan-calculator.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-loan-calculator',
   templateUrl: './loan-calculator.component.html',
   styleUrls: ['./loan-calculator.component.css']
 })
-export class LoanCalculatorComponent implements OnInit {
+export class LoanCalculatorComponent {
+  loanAmount: number | null = null;
+  interestRate: number | null = null;
+  loanTerm: number | null = null;
+  monthlyPayment: number | null = null;
+  totalInterest: number | null = null;
 
-  constructor() { }
+  calculateLoan(): void {
+    if (this.loanAmount && this.interestRate && this.loanTerm) {
+      const principle = this.loanAmount;
+      const ratePerPeriod = this.interestRate / 12 / 100;
+      const numPayments = this.loanTerm * 12;
 
-  ngOnInit(): void {
+      const numerator = principle * (ratePerPeriod * Math.pow(1 + ratePerPeriod, numPayments));
+      const denominator = Math.pow(1 + ratePerPeriod, numPayments) - 1;
+
+      this.monthlyPayment = numerator / denominator;
+      this.totalInterest = this.monthlyPayment * numPayments - principle;
+    }
   }
-
 }
+
